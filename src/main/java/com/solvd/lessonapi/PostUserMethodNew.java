55be80cec12f14d7d1fb171912adc12176f20fde
knovash <novash.ki@gmail.com>
2022-10-24 09:58:24 +0300

    new method removed constructor

--- src/main/java/com/solvd/lessonapi/PostUserMethodNew.java
+++ src/main/java/com/solvd/lessonapi/PostUserMethodNew.java
@@ -2,14 +2,13 @@ package com.solvd.lessonapi;
 
 import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
 import com.qaprosoft.carina.core.foundation.api.annotation.Endpoint;
+import com.qaprosoft.carina.core.foundation.api.annotation.RequestTemplatePath;
+import com.qaprosoft.carina.core.foundation.api.annotation.ResponseTemplatePath;
 import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 
-@Endpoint(url = "${base_url}/users", methodType = HttpMethodType.POST)
+@Endpoint(url = "${config.env.url}/users", methodType = HttpMethodType.POST)
+@RequestTemplatePath(path = "/api/users/get/rq.json")
+@ResponseTemplatePath(path = "/api/users/get/rs.json")
 public class PostUserMethodNew extends AbstractApiMethodV2 {
-
-    public PostUserMethodNew() {
-        super("/api/users/get/rq.json", "/api/users/get/rs.json");
-        replaceUrlPlaceholder("base_url", Configuration.getEnvArg("url"));
-    }
 }
